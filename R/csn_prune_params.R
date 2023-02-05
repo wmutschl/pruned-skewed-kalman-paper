a58b537e66cdbfb079816ab629390b928c19a47c
Willi Mutschler <willi@mutschler.eu>
2023-02-05 21:13:30 +0100

    Added code for figure 3

---
+++ R/csn_prune_params.R
@@ -0,0 +1,55 @@
+pruned_csn_params <- function(Sigma,Gamma,nu,Delta, tol=0.1){
+    # pruned_csn_params <- function(Sigma,Gamma,nu,Delta, tol=0.1)
+    # -------------------------------------------------------------------------
+    # compute pruned parameters of CSN(mu,Sigma,Gamma,nu,Delta) distribution
+    # as proposed in the paper
+    # "Pruned Skewed Kalman Filter and Smoother: With Application to the Yield Curve"
+    # by Gaygysyz Guljanov, Willi Mutschler, Mark Trede
+    # =========================================================================
+    # INPUTS
+    #   - Sigma: scale parameter of CSN distribution, [p x p] matrix
+    #   - Gamma: first skewness parameter of CSN distribution, [q x p] matrix
+    #   - nu   : second skewness parameter of CSN distribution, [q x 1] vector
+    #   - Delta: third skewness parameter of CSN distribution, [q x 1] matrix
+    #   - tol  : threshold at which correlations are pruned, scalar between 0 and 1
+    # -------------------------------------------------------------------------
+    # OUTPUTS
+    #   - pruned_csn_params: list of pruned parameters Sigma, Gamma, nu, Delta
+    # =========================================================================
+    # Copyright © 2022-2023 Gaygysyz Guljanov, Willi Mutschler
+    #
+    # This is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+    #
+    # This is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+    # =========================================================================
+    p <- dim(Sigma)[1]
+    q <- dim(Delta)[1]
+    P1 <- Sigma
+    P2 <- Sigma %*% t(Gamma)
+    P3 <- Gamma %*% Sigma
+    P4 <- Delta+Gamma %*% Sigma %*% t(Gamma)
+    P <- rbind(cbind(P1,P2), cbind(P3,P4))
+    maxval <- apply(cov2cor(P)[(p+1):(p+q), 1:p, drop=F], 1, max)
+    cutvector <- (maxval < tol)
+    if(all(cutvector)){
+        Gamma <- matrix(0,1,1)
+        nu <- 0
+        Delta <- matrix(1,1,1)
+    } else if(any(cutvector)){
+        P3 <- P3[!cutvector,,drop=F]
+        P4 <- P4[!cutvector,!cutvector,drop=F]
+        Gamma <- P3 %*% solve(Sigma)
+        nu <- nu[!cutvector]
+        Delta <- P4 - P3 %*% t(Gamma)
+    }
+    return(list(Sigma=Sigma,
+                Gamma=Gamma,
+                nu=nu,
+                Delta=Delta))
+}
---
+++ figure3.R
@@ -0,0 +1,58 @@
+# Replicates Figure 3: "Probability density functions and cumulative distribution
+# functions of a CSN distributed random variable with two skewness dimensions
+# and the approximating pruned CSN distribution with one skewness dimenstions"
+# of the paper
+# "Pruned Skewed Kalman Filter and Smoother: With Application to the Yield Curve"
+# by Gaygysyz Guljanov, Willi Mutschler, Mark Trede
+# =========================================================================
+# Copyright © 2022-2023 Gaygysyz Guljanov, Willi Mutschler, Mark Trede
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# =========================================================================
+library(ggplot2)
+library(gridExtra)
+library(csn)
+source("R/csn_prune_params.R")
+
+Sigma <- matrix(1,1,1)
+Gamma <- matrix(c(6,0.1),2,1)
+nu <- matrix(c(0,0),2,1)
+Delta <- matrix(c(1,-0.1,-0.1,1),2,2)
+
+P <- rbind(cbind(Sigma,Sigma%*%t(Gamma)),cbind(Gamma%*%Sigma, Delta+Gamma%*%Sigma%*%t(Gamma)))
+R <- cov2cor(P)
+R
+PrunedParams <- pruned_csn_params(Sigma,Gamma,nu,Delta)
+
+Sigma_pruned <- PrunedParams$Sigma
+Gamma_pruned <- PrunedParams$Gamma
+nu_pruned <- PrunedParams$nu
+Delta_pruned <- PrunedParams$Delta
+
+x <- seq(-1,4,length=200)
+H <- data.frame(x=x,
+                f1=dcsn(x,0,Sigma,        Gamma,        as.vector(nu),        Delta       ),
+                f2=dcsn(x,0,Sigma_pruned, Gamma_pruned, as.vector(nu_pruned), Delta_pruned),
+                c1=pcsn(x,0,Sigma,        Gamma,        as.vector(nu),        Delta       ),
+                c2=pcsn(x,0,Sigma_pruned, Gamma_pruned, as.vector(nu_pruned), Delta_pruned)
+                )
+
+p1 <- ggplot(H, aes(x,f1)) +
+        geom_line()+
+        geom_line(aes(y=f2),linetype="dashed")+
+        ylab("probability density function")
+
+p2 <- ggplot(H, aes(x,c1)) +
+        geom_line()+
+        geom_line(aes(y=c2),linetype="dashed")+
+        ylab("cumulative distribution function")
+
+grid.arrange(p1,p2,ncol=2)
