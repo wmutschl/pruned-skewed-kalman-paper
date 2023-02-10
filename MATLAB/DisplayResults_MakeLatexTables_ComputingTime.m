5d9c6e5b3413c7e7d4db7dc07e5e1ba08bbbe2dc
Willi Mutschler <willi@mutschler.eu>
2023-02-10 11:05:37 +0100

    Added code to replicate table 3

---
+++ MATLAB/DisplayResults_MakeLatexTables_ComputingTime.m
@@ -0,0 +1,127 @@
+function DisplayResults_MakeLatexTables_ComputingTime(filename,ONLY_LATEX)
+% DisplayResults_MakeLatexTables_ComputingTime(filename)
+% -------------------------------------------------------------------------
+% Collects and displays the results of the Monte-Carlo assessment of the 
+% computing time of the log-likelihood function with both the Gaussian
+% as well as Pruned Skewed Kalman filter in Computing_Time_Log_Likliheood.m
+% Creates a log file with results and also the Latex code for the rows of
+% Table 3 of the paper "Pruned Skewed Kalman Filter and Smoother: With Application
+% to the Yield Curve" by Gaygysyz Guljanov, Willi Mutschler, Mark Trede
+% -------------------------------------------------------------------------
+% INPUTS
+% - filename   [string]   load results created with Computing_Time_Log_Likliheood.m and saved in results/"filename".mat,
+%                         for example: results_computing_time_DGP1_T50_R1000_maca64
+% - ONLY_LATEX [boolean]  only print the latex rows in the command window
+% -------------------------------------------------------------------------
+% OUTPUTS
+% - results are displayed in the command window
+% - latex codes are displayed in the command window
+% - a log file "filename.log" is created in the results folder
+% =========================================================================
+% Copyright (C) 2022-2023 Gaygysyz Guljanov, Willi Mutschler, Mark Trede
+%
+% This is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% -------------------------------------------------------------------------
+% This file is part of the replication files for the paper "Pruned Skewed
+% Kalman Filter and Smoother: With Application to the Yield Curve" by
+% Gaygysyz Guljanov, Willi Mutschler, Mark Trede (2022)
+% =========================================================================
+if nargin < 2
+    ONLY_LATEX = false;
+end
+
+load(['results/' filename],'OPT','PARAMS','RES');
+
+%% start log file
+if ~ONLY_LATEX
+    diary off
+    OPT.logfile = ['results/' filename,'.log'];
+    if exist(OPT.logfile, 'file')
+        delete(OPT.logfile)
+    end
+    diary(OPT.logfile)
+end
+
+if ~ONLY_LATEX
+    format long
+    disp(repmat('*',1,60));
+    % print settings to logfile
+    disp(OPT)
+    disp(repmat('*',1,60));
+    disp('Parameter Matrix G:'); disp(PARAMS.G);
+    disp('Parameter Matrix F:'); disp(PARAMS.F);
+    disp('Parameter Matrix R:'); disp(PARAMS.R);
+    disp('Parameter Matrix mu_eps:');    disp(PARAMS.mu_eps);
+    disp('Parameter Matrix Sigma_eps:'); disp(PARAMS.Sigma_eps);
+    disp('Parameter Matrix mu_eta:');         disp(PARAMS.mu_eta);
+    disp('Parameter Matrix Sigma_eta:');      disp(PARAMS.Sigma_eta);
+    disp('Parameter Matrix nu_eta:');         disp(PARAMS.nu_eta);
+    disp('Parameter Matrix Gamma_eta:');      disp(PARAMS.Gamma_eta);
+    disp('Parameter Matrix Delta_eta:');      disp(PARAMS.Delta_eta);
+    disp('Parameter Matrix lambda_eta:');     disp(PARAMS.lambda_eta);
+    disp('Parameter Matrix sqrt_Sigma_eta:'); disp(PARAMS.sqrt_Sigma_eta);
+    disp(repmat('*',1,60));
+    format short
+end
+
+%% table with summary statistics
+if ~ONLY_LATEX
+    tblTitles.SampleStats = {};
+    for j=1:size(PARAMS.F,1)
+        tblTitles.SampleStats = [tblTitles.SampleStats sprintf('mean(y%d)',j) sprintf('sd(y%d)',j) sprintf('skew(y%d)',j)];
+    end
+    fprintf('%s\n%sSUMMARY STATISTICS\n',repmat('*',1,60),repmat(' ',1,10));
+    disp(array2table(RES.SampleStats,'VariableNames',tblTitles.SampleStats));
+end
+
+%% table with computing time
+Time     = [RES.Time.gauss RES.Time.csn]*1000; % in miliseconds
+MeanTime = mean(Time,1);
+LowTime  = quantile(Time,0.05,1);
+HighTime = quantile(Time,0.95,1);
+RowNamesLoss = [sprintf("Mean") sprintf("Low") sprintf("High")];
+
+if ~ONLY_LATEX
+    fprintf('%s\nTime to compute the log-likelihood once (in ms)\n',repmat('*',1,60))
+    tblTitles.Time = {'Gaussian'};
+    for j=1:size(OPT.prune_tol,2)
+        tblTitles.Time = [tblTitles.Time sprintf('CSN(%.d)',OPT.prune_tol(j))];
+    end
+    format long;
+    disp(array2table([MeanTime;LowTime;HighTime],'VariableNames',tblTitles.Time,'RowNames',RowNamesLoss'));
+    format short;
+    disp(['Total computing time : ' dynsec2hms(RES.time_Computing_Time) ]);
+end
+
+%% Latex table
+if ~ONLY_LATEX
+    fprintf('\n\nLatex Table Entries:\n\n')
+end
+if OPT.prune_tol(1) ~= 0
+    MeanTime = [MeanTime(:,1) zeros(size(MeanTime,1),1) MeanTime(:,2:end)];
+    LowTime  = [LowTime(:,1)  zeros(size(LowTime,1),1)  LowTime(:,2:end)];
+    HighTime = [HighTime(:,1) zeros(size(HighTime,1),1) HighTime(:,2:end)];
+end
+tbl_latex = [...
+            sprintf('%s & %d & $\\underset{[%.2f;%.2f]}{%.4f}$ & $\\underset{[%.2f;%.2f]}{%.4f}$ & $\\underset{[%.2f;%.2f]}{%.4f}$ & $\\underset{[%.2f;%.2f]}{%.4f}$ & $\\underset{[%.2f;%.2f]}{%.4f}$ \\\\'...
+            ,OPT.parameter_set...
+            ,OPT.sample_size...
+            ,LowTime(1,1), HighTime(1,1), MeanTime(1,1)...
+            ,LowTime(1,2), HighTime(1,2), MeanTime(1,2)...
+            ,LowTime(1,3), HighTime(1,3), MeanTime(1,3)...
+            ,LowTime(1,4), HighTime(1,4), MeanTime(1,4)...
+            ,LowTime(1,5), HighTime(1,5), MeanTime(1,5)...
+            );];
+disp(tbl_latex);
+
+if ~ONLY_LATEX
+    diary off
+end
---
+++ table3.m
@@ -0,0 +1,177 @@
+% Replicates:
+% - Table 3: "Time in seconds to compute 1000 evaluations of the log-likelihood function"
+% of the paper "Pruned Skewed Kalman Filter and Smoother: With Application
+% to the Yield Curve" by Gaygysyz Guljanov, Willi Mutschler, Mark Trede
+% -------------------------------------------------------------------------
+% The results are saved both as MAT as well as log files in the "results" folder
+% -------------------------------------------------------------------------
+% Note that we use the following re-parametrization for the skewness parameters of the CSN distributed innovations eta:
+%   - Gamma_eta = lambda_eta*Sigma_eta^(-1/2)
+%   - nu_eta = zeros(nx,1)
+%   - Delta_eta = (1-lambda^2)*eye(nx)
+% where lambda=]-1;1[ (together with Sigma_eta) scales the skewness of the distribution and nu_eta=0 for lack of identification.
+% This re-parametrization enables one to compute the theoretical moments easily in closed-form (alternatively you can make use of csn_mean and csn_var,
+% which rely on numerical approximations of the Jacobian and Hessian of certain Gaussian CDF functions)
+% =========================================================================
+% Copyright © 2022-2023 Gaygysyz Guljanov, Willi Mutschler, Mark Trede
+%
+% This is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% =========================================================================
+
+%% Settings
+
+% Housekeeping
+clearvars; clearvars -global; clc; close all;
+addpath('MATLAB');
+
+REDO = 3; % 0: only create tables, 1: redo DGP1, 2: redo DGP2, 3: redo DGP1 and DGP2
+
+% Common options
+SAMPLE_SIZES           = 50:50:250;          % loop over different sample lengths
+OPT0.mc_replic         = 1000;               % number of Monte Carlo replications
+OPT0.prune_tol         = [0 1e-6 1e-4 1e-2]; % pruning thresholds
+OPT0.data_burnin       = 100;                % periods to discard in simulations
+OPT0.Harvey_factor     = 10;                 % factor for wide prior for Kalman filter initialization, as suggested by Harvey and Phillips(1979)
+OPT0.cdfmvna_fct       = "logmvncdf_ME";     % function to use to evaluate high-dimensional Gaussian log cdf, possible options: "logmvncdf_ME", "mvncdf", "qsilatmvnv", "qsimvnv"
+
+% Parallel environment
+if REDO > 0
+    OPT0.poolobj = gcp('nocreate');
+    if isempty(OPT0.poolobj)
+        OPT0.physical_cores_nbr=feature('numcores');
+        parpool(OPT0.physical_cores_nbr);
+    else
+        OPT0.physical_cores_nbr = OPT0.poolobj.NumWorkers;
+    end
+end
+OPT0.computer_arch = computer('arch');
+
+if ~exist('results', 'dir')
+    mkdir('results');
+end
+
+%% Monte-Carlo study for univariate DGP (1)
+if REDO == 1 || REDO == 3
+    clear OPT PARAMS RES sample_size
+
+    OPT = OPT0;
+    OPT.parameter_set = 'DGP1';
+    OPT.seed_nbr      = 4112460544; % randi([0 2^32],1,1); % draw random seed
+    
+    PARAMS.G          = 0.8;   % matrix multiplying previous states in state transition equation
+    PARAMS.R          = 1;     % matrix multiplying innovations in state transition equation
+    PARAMS.F          = 10;    % matrix multiplying current states in measurement equation
+    PARAMS.mu_eps     = 1;     % expectation of Gaussian noise
+    PARAMS.Sigma_eps  = 1e-2;  % variance of Gaussian noise
+    PARAMS.mu_eta     = 0.3;   % location parameter of CSN distributed innovation eta(t)
+    PARAMS.Sigma_eta  = 0.64;  % scale parameter of CSN distributed innovation eta(t)
+    PARAMS.lambda_eta = -0.89; % auxiliary skewness parameter, between -1 and 1, that determines Gamma_eta and Delta_eta below
+    % compute skewness parameters Gamma_eta, nu_eta, Delta_eta from re-parametrization values
+    [U,S,V] = svd(PARAMS.Sigma_eta); PARAMS.sqrt_Sigma_eta = U*diag(diag(S).^(1/2))*V'; PARAMS.inv_sqrt_Sigma_eta = U*diag(diag(S).^(-1/2))*V'; % compute Sigma_eta^(-1/2)
+    PARAMS.Gamma_eta = PARAMS.lambda_eta*PARAMS.inv_sqrt_Sigma_eta; % first skewness matrix of CSN distributed innovations eta(t), making use of re-parametrization
+    PARAMS.nu_eta = zeros(size(PARAMS.Gamma_eta,1),1); % second skewness matrix of CSN distributed innovations eta(t), fix at 0 due to re-parametrization and lack of identification
+    PARAMS.Delta_eta = (1-PARAMS.lambda_eta^2)*eye(size(PARAMS.Gamma_eta,1));
+    if (norm(PARAMS.Delta_eta + PARAMS.Gamma_eta'*PARAMS.Sigma_eta*PARAMS.Gamma_eta - eye(size(PARAMS.nu_eta,1)),'Inf')>1e-7)
+        error('Reparametrization failed')
+    end
+    clear U S V;
+
+    % loop over sample sizes and perform Monte Carlo analysis
+    for sample_size = SAMPLE_SIZES
+        OPT.sample_size = sample_size;
+        fprintf('%s with T=%d and R=%d\n',OPT.parameter_set,OPT.sample_size,OPT.mc_replic);
+        tic
+        [RES.Time,RES.SampleStats] = Computing_Time_Log_Likelihood(PARAMS,OPT); % simulates data and computes the log-likelihood function
+        RES.time_Computing_Time = toc;
+        OPT.filename = sprintf('results_computing_time_%s_T%d_R%d_%s',OPT.parameter_set,OPT.sample_size,OPT.mc_replic,OPT.computer_arch);
+        save(['results/' OPT.filename],'OPT','PARAMS','RES');
+    end
+end
+
+%% Monte-Carlo study for multivariate DGP (2)
+if REDO == 2 || REDO == 3
+    clear OPT PARAMS RES sample_size
+
+    OPT = OPT0;
+    OPT.parameter_set = 'DGP2';
+    OPT.seed_nbr      = 1936362161; %randi([0 2^32],1,1); % draw random seed
+    
+    rng(OPT.seed_nbr);
+    y_nbr = 3; x_nbr = 4;
+    is_stationary = 0;
+    while is_stationary == 0
+        PARAMS.G = 80*randn(x_nbr); % matrix multiplying previous states in state transition equation
+        [v,d] = eig(PARAMS.G);
+        PARAMS.G = real(v*diag(-1+2.*rand(length(PARAMS.G),1))/v);
+        PARAMS.G = round(PARAMS.G,4);
+        eigG = eig(PARAMS.G);
+        if all(imag(eigG)==0) && all(abs(eigG)<1)
+            is_stationary=1;
+        end
+    end
+    PARAMS.R = eye(x_nbr);                    % matrix multiplying innovations in state transition equation
+    PARAMS.F = round(randn(y_nbr, x_nbr),4);  % matrix multiplying current states in measurement equation
+    PARAMS.mu_eps = round(randn(y_nbr,1),4);  % expectation of Gaussian noise
+    PARAMS.Sigma_eps = round(tril(0.5*randn(y_nbr)),2); PARAMS.Sigma_eps = PARAMS.Sigma_eps*PARAMS.Sigma_eps'./1e6; % variance of Gaussian noise
+    PARAMS.mu_eta = round(randn(x_nbr,1),4);  % location parameter of CSN distributed innovation eta(t)
+    PARAMS.Sigma_eta = round(tril(5*randn(x_nbr)),2); PARAMS.Sigma_eta = PARAMS.Sigma_eta*PARAMS.Sigma_eta';        % scale parameter of CSN distributed innovation eta(t)
+    while max(abs(PARAMS.Sigma_eta(:))) > 10
+        PARAMS.Sigma_eta = PARAMS.Sigma_eta./10;
+    end
+    PARAMS.lambda_eta = 0.89; % auxiliary skewness parameter, between -1 and 1, that determines Gamma_eta and Delta_eta below
+    % compute skewness parameters Gamma_eta, nu_eta, Delta_eta from re-parametrization values
+    [U,S,V] = svd(PARAMS.Sigma_eta); PARAMS.sqrt_Sigma_eta = U*diag(diag(S).^(1/2))*V'; PARAMS.inv_sqrt_Sigma_eta = U*diag(diag(S).^(-1/2))*V'; % compute Sigma_eta^(-1/2)
+    PARAMS.Gamma_eta = PARAMS.lambda_eta*PARAMS.inv_sqrt_Sigma_eta; % first skewness matrix of CSN distributed innovations eta(t), making use of re-parametrization
+    PARAMS.nu_eta = zeros(size(PARAMS.Gamma_eta,1),1); % second skewness matrix of CSN distributed innovations eta(t), fix at 0 due to re-parametrization and lack of identification
+    PARAMS.Delta_eta = (1-PARAMS.lambda_eta^2)*eye(size(PARAMS.Gamma_eta,1));
+    if (norm(PARAMS.Delta_eta + PARAMS.Gamma_eta'*PARAMS.Sigma_eta*PARAMS.Gamma_eta - eye(size(PARAMS.nu_eta,1)),'Inf')>1e-7)
+        error('Reparametrization failed')
+    end
+    clear v d eigG is_stationary U S V x_nbr y_nbr
+
+    % loop over sample sizes and perform Monte Carlo analysis
+    for sample_size = SAMPLE_SIZES
+        OPT.prune_tol = OPT0.prune_tol;
+        if sample_size > 100 && any(ismember(OPT.prune_tol,0))
+            OPT.prune_tol(ismember(OPT.prune_tol,0)) = []; % remove the uncut version in multivariate case
+            warning('In the multivariate case, we do not compute the unpruned version for sample sizes larger than 100. I remove the 0 in prune_tol.')
+        end
+        OPT.sample_size = sample_size;
+        fprintf('%s with T=%d and R=%d\n',OPT.parameter_set,OPT.sample_size,OPT.mc_replic);
+        tic        
+        [RES.Time,RES.SampleStats] = Computing_Time_Log_Likelihood(PARAMS,OPT); % simulates data and computes the log-likelihood function
+        RES.time_Computing_Time = toc;
+        OPT.filename = sprintf('results_computing_time_%s_T%d_R%d_%s',OPT.parameter_set,OPT.sample_size,OPT.mc_replic,OPT.computer_arch);
+        save(['results/' OPT.filename],'OPT','PARAMS','RES');
+    end
+end
+
+%% display all results and create tables for paper
+ONLY_LATEX = false;
+%OPT0.computer_arch = 'glnxa64';
+%OPT0.computer_arch = 'maca64';
+%OPT0.computer_arch = 'maci64';
+%OPT0.computer_arch = 'win64';
+DisplayResults_MakeLatexTables_ComputingTime(['results_computing_time_DGP1_T50_R1000_'  OPT0.computer_arch],ONLY_LATEX);
+DisplayResults_MakeLatexTables_ComputingTime(['results_computing_time_DGP1_T100_R1000_' OPT0.computer_arch],ONLY_LATEX);
+DisplayResults_MakeLatexTables_ComputingTime(['results_computing_time_DGP1_T150_R1000_' OPT0.computer_arch],ONLY_LATEX);
+DisplayResults_MakeLatexTables_ComputingTime(['results_computing_time_DGP1_T200_R1000_' OPT0.computer_arch],ONLY_LATEX);
+DisplayResults_MakeLatexTables_ComputingTime(['results_computing_time_DGP1_T250_R1000_' OPT0.computer_arch],ONLY_LATEX);
+
+DisplayResults_MakeLatexTables_ComputingTime(['results_computing_time_DGP2_T50_R1000_'  OPT0.computer_arch],ONLY_LATEX);
+DisplayResults_MakeLatexTables_ComputingTime(['results_computing_time_DGP2_T100_R1000_' OPT0.computer_arch],ONLY_LATEX);
+DisplayResults_MakeLatexTables_ComputingTime(['results_computing_time_DGP2_T150_R1000_' OPT0.computer_arch],ONLY_LATEX);
+DisplayResults_MakeLatexTables_ComputingTime(['results_computing_time_DGP2_T200_R1000_' OPT0.computer_arch],ONLY_LATEX);
+DisplayResults_MakeLatexTables_ComputingTime(['results_computing_time_DGP2_T250_R1000_' OPT0.computer_arch],ONLY_LATEX);
+
+
+%% Clean up
+rmpath('MATLAB');
