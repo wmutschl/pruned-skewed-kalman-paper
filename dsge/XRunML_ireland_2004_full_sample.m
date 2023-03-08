a730ad6069e449e06431b5556dd2e026ea4ba9ec
Willi Mutschler <willi@mutschler.eu>
2023-03-08 08:59:26 +0100

    //wip

--- dsge/XRunML_ireland_2004_full_sample.m
+++ dsge/XRunML_ireland_2004_full_sample.m
@@ -6,8 +6,8 @@ addpath('../MATLAB/OptimRoutines/CMAES','../MATLAB/OptimRoutines/CSMINWEL'); % t
 
 %% Options
 OPT.modelname = "ireland_2004";
-%OPT.datafile  = "data_full_sample";
-OPT.datafile = "data_post_1980";
+OPT.datafile  = "data_full_sample";
+%OPT.datafile = "data_post_1980";
 %OPT.datafile = "data_pre_1980";
 OPT.optimizer.randomize_initval         = 0; % 1: randomize initial values
 OPT.optimizer.bounds.do_param_transform = 0; % 1: transforms parameters with bounded support into parameters with unbounded support by using either exp() or logistic transformation
@@ -182,7 +182,6 @@ stderr_eta_1
 skew_eta_1
 
 %% Stage 2:
-% for each of the best 5 parameter combinations: re-run ML optimization of all model parameters (initialized at Gaussian values) and of Sigma_eta and Gamma_eta (initialized at best Sigma_eta_j and Gamma_eta_j of the grid)
 MODEL_2 = MODEL; MODEL_2.param_estim_names = MODEL_0.param_estim_names; OPT_2 = OPT; OPT_2.optimizer.optim_options.Display = 'iter';
 if OPT.use_stderr_skew_transform
     [xparam_2, PARAM_2, ESTIM_PARAM_2, MODEL_2, OPT_2] = feval(str2func(OPT_2.modelname + "_params"),  2, MODEL_2, OPT_2, xparam_0, [], [], stderr_eta_1, skew_eta_1);
@@ -218,164 +217,6 @@ hess = get_hessian('negative_log_likelihood_dsge',xparam_2,[1e-3;1.0],    DATA.M
 hess = reshape(hess,MODEL_2.param_estim_nbr,MODEL_2.param_estim_nbr);
 V = inv(hess); % estimated covariance matrix of coefficients
 SE_values = sqrt(diag(V));
-
-
-%     [xparam_1_j, PARAM_1_j, ESTIM_PARAM_1_j, MODEL_1_j, OPT_1_j] = feval(str2func(OPT_1.modelname + "_params"),  1, MODEL_1, OPT_1, xparam_0, sqrt_Sigma_eta_j, diag_Gamma_eta_j);
-% neg_log_likelihood_grid_1(idx_best_1(1:5))
-% gamma_eta_grid = gamma_eta_grid(:,idx_best(1:24));
-% neg_log_likelihood_grid = nan(1,size(gamma_eta_grid,2));
-% x_grid = nan(MODEL.param_estim_nbr,size(gamma_eta_grid,2));
-% 
-% objective_function = @(x) negative_log_likelihood_dsge(x,DATA.MAT,PARAM,MODEL,OPT);
-% parfor_progress(size(gamma_eta_grid,2));
-% parfor jgrid = 1:size(gamma_eta_grid,2)
-%     xpar0 = xparam_0;
-%     xpar0(2) = gamma_eta_grid(1,jgrid);
-%     xpar0(4) = gamma_eta_grid(2,jgrid);
-%     xpar0(6) = gamma_eta_grid(3,jgrid);
-%     xpar0(8) = gamma_eta_grid(4,jgrid);
-%     [x_grid(:,jgrid),neg_log_likelihood_grid(:,jgrid)] = fminsearch(objective_function,xpar0,OPT.optimizer.optim_options);
-%     parfor_progress;
-% end
-% parfor_progress(0);
-% 
-% for jgrid = 1:size(gamma_eta_grid,2)
-%     Skew_eta_grid(1,jgrid) = skewness_coef_theor(x_grid(1,jgrid),x_grid(2,jgrid));
-%     Skew_eta_grid(2,jgrid) = skewness_coef_theor(x_grid(3,jgrid),x_grid(4,jgrid));
-%     Skew_eta_grid(3,jgrid) = skewness_coef_theor(x_grid(5,jgrid),x_grid(6,jgrid));
-%     Skew_eta_grid(4,jgrid) = skewness_coef_theor(x_grid(7,jgrid),x_grid(8,jgrid));
-% end
-% Skew_eta_grid
-% [~,idx_best] = sort(neg_log_likelihood_grid);
-% neg_log_likelihood_grid(idx_best)
-% Skew_eta_grid(:,idx_best)
-% x_grid(1,idx_best)
-% x_grid(2,idx_best)
-% x_grid(3,idx_best)
-% x_grid(4,idx_best)
-% x_grid(5,idx_best)
-% x_grid(6,idx_best)
-% x_grid(7,idx_best)
-% x_grid(8,idx_best)
-%     %% check objective function at initial parameters
-%     if OPT.optimizer.randomize_initval
-%     end
-%     
-%     if OPT.optimizer.bounds.do_param_transform    
-%     end
-%     
-%     % compute, check and print objective function
-%     tic_id = tic;
-%     [neg_log_likelihood,exit_flag] = negative_log_likelihood_dsge(xpar0,DATA.MAT,PARAM,MODEL,OPT);
-%     elapsed_time = toc(tic_id);
-%     if exit_flag ~= 1
-%         error('Something wrong with log-likelihood function at initial parameters')
-%     end
-%     fprintf('Initial value of the log-likelihood function: %6.4f \n', -1*neg_log_likelihood);
-%     fprintf('Time required to compute log-likelihood function once: %5.4f seconds \n', elapsed_time);
-%     
-%     
-%     %% Maximum Likelihood Estimation with different optimizers
-%     if MODEL.param_estim_nbr > 0
-%     
-%         % initialize output structure, columns are for different optimizers
-%         ML.f  = nan(1,size(OPT.optimizer.name,2)); % stores function value
-%         ML.x  = nan(MODEL.param_estim_nbr,size(OPT.optimizer.name,2)); % stores optimized parameters
-%         ML.se = nan(MODEL.param_estim_nbr,size(OPT.optimizer.name,2)); % stores standard errors of optimized parameters
-%         
-%         % deal with bounds
-%         if OPT.optimizer.bounds.use_for_optimizer
-%             LB = OPT.optimizer.bounds.lb;
-%             UB = OPT.optimizer.bounds.ub;
-%         else
-%             LB = -Inf*ones(MODEL.param_estim_nbr,1);
-%             UB = Inf*ones(MODEL.param_estim_nbr,1);
-%         end
-%         
-%         % pass additional arguments to objective function using an anonymous function
-%         objective_function = @(x) negative_log_likelihood_dsge(x,DATA.MAT,PARAM,MODEL,OPT);
-%         
-%         for jopt = 1:size(OPT.optimizer.name,2)            
-%             tic_opt = tic;
-%             switch OPT.optimizer.name(jopt)
-%                 case 'fminsearch' % does not care about bounds
-%                     [xparam1,fval] = fminsearch(objective_function,xpar0,OPT.optimizer.optim_options);
-%                 case 'fminunc' % does not care about bounds
-%                     [xparam1,fval] = fminunc(objective_function,xpar0,OPT.optimizer.optim_options);
-%                 case 'patternsearch' % does care about bounds
-%                     [xparam1,fval] = patternsearch(objective_function,xpar0,[],[],[],[],LB,UB,[],OPT.optimizer.optim_options);
-%                 case 'csminwel' % does not care about bounds
-%                     [fval,xparam1] = csminwel(objective_function,xpar0,1e-4*eye(MODEL.param_estim_nbr),[],OPT.optimizer.optim_options.TolFun,OPT.optimizer.optim_options.MaxIter);
-%                 case 'simulannealbnd' % does care about bounds
-%                     [xparam1,fval] = simulannealbnd(objective_function,xpar0,LB,UB,OPT.optimizer.optim_options);
-%                 case 'cmaes' % does care about bounds
-%                     cmaesOptions.SaveVariables='on'; cmaesOptions.DispFinal='on'; cmaesOptions.WarnOnEqualFunctionValues='no'; cmaesOptions.DispModulo='10'; cmaesOptions.LogModulo='0'; cmaesOptions.LogTime='0'; cmaesOptions.Resume = 0;
-%                     cmaesOptions.TolFun = OPT.optimizer.optim_options.TolFun;
-%                     cmaesOptions.TolX = OPT.optimizer.optim_options.TolX;
-%                     cmaesOptions.MaxIter = OPT.optimizer.optim_options.MaxIter;
-%                     cmaesOptions.MaxFunEvals = OPT.optimizer.optim_options.MaxFunEvals;        
-%                     cmaesOptions.LBounds = LB;
-%                     cmaesOptions.UBounds = UB;
-%                     % Set default search volume (SIGMA)            
-%                     cmaesSIGMA = (UB-LB)*0.2;
-%                     cmaesSIGMA(~isfinite(cmaesSIGMA)) = 0.01;
-%                     while max(cmaesSIGMA)/min(cmaesSIGMA)>1e6 %make sure initial search volume (SIGMA) is not badly conditioned
-%                         cmaesSIGMA(cmaesSIGMA==max(cmaesSIGMA))=0.9*cmaesSIGMA(cmaesSIGMA==max(cmaesSIGMA));
-%                     end
-%                     %warning('off','CMAES:NonfinitenessRange');
-%                     %warning('off','CMAES:InitialSigma');
-%                     [~, ~, ~, ~, ~, BESTEVER] = cmaes('negative_log_likelihood_dsge',xpar0,cmaesSIGMA,cmaesOptions,...
-%                                                                               DATA.MAT, PARAM, MODEL, OPT);
-%                     xparam1=BESTEVER.x;
-%                     fval=BESTEVER.f;
-%             end
-%             elapsed_time_opt = toc(tic_opt);
-%             % Retransform parameters if needed
-%             if OPT.optimizer.bounds.do_param_transform
-%                 xopt = param_transform_bounded(xparam1,OPT.optimizer.bounds.lb,OPT.optimizer.bounds.ub);
-%             else
-%                 xopt = xparam1;        
-%             end
-%             % Compute standard errors
-%             old_param_transform = OPT.optimizer.bounds.do_param_transform;
-%             OPT.optimizer.bounds.do_param_transform = 0; %turn off for computing standard errors
-%             OPT.optimizer.bounds.lb = OPT.optimizer.bounds.lb -0.5;
-%             OPT.optimizer.bounds.ub = OPT.optimizer.bounds.ub + 0.5;
-%             hess = get_hessian('negative_log_likelihood_dsge',xopt,[1e-3;1.0],    DATA.MAT, PARAM, MODEL, OPT);
-%             hess = reshape(hess,MODEL.param_estim_nbr,MODEL.param_estim_nbr);
-%             V = inv(hess); % estimated covariance matrix of coefficients
-%             SE_values = sqrt(diag(V));
-%             OPT.optimizer.do_param_transform = old_param_transform; %turn back on
-%         
-%             % store into output structure
-%             ML.f(jopt) = fval;
-%             ML.x(:,jopt) = xopt;
-%             ML.se(:,jopt) = SE_values;
-%             ML.elapsed_time(jopt) = elapsed_time_opt;
-%             disp( table(ML.x(:,jopt),ML.se(:,jopt),ML.x(:,jopt)./ML.se(:,jopt),'Var',{'Estimate','s.d.','t-stat'},'Row',MODEL.param_estim_names) );
-%             fprintf('Value of maximimized log-likelihood function: %12.10f.\n',-ML.f(jopt));
-%             fprintf('Time required: %5.4f seconds \n', ML.elapsed_time(jopt));    
-%             %input(sprintf('Finished optimization with %s. Hit enter for next optimizer',OPT.optimizer.name(jopt)));
-%         end
-%         
-%         % Display estimation results
-%         clc; % clear screen
-%         [~,jbest] = max(-ML.f);
-%         fprintf('SUMMARY\n\n')
-%         for jopt=1:size(OPT.optimizer.name,2)
-%             if jopt == jbest
-%                 fprintf('BEST ')
-%             end
-%             fprintf('Optimizer %s: ',OPT.optimizer.name(jopt));
-%             fprintf('value of maximimized log-likelihood function: %12.10f.\n',-ML.f(jopt));
-%             fprintf('Time required: %5.4f seconds \n', ML.elapsed_time(jopt));
-%             disp( table(ML.x(:,jopt),ML.se(:,jopt),ML.x(:,jopt)./ML.se(:,jopt),'Var',{'Estimate','s.d.','t-stat'},'Row',MODEL.param_estim_names) );
-%         end 
-%     end
-%     neg_log_likelihood_grid(jgrid) = ML.f(jopt);
-%     x_grid(:,jgrid) = xopt;
-% end % jgrid
-
 
 % housekeeping
 rmpath('../MATLAB'); % this folder contains our core routines
